4c540da348bc8b66e33f023371fcf435cd05aa73
vision800 <netshokotshokovision112@gmail.com>
2024-01-10 10:18:27 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book book = new Book("Faith hub","vision",45);
+
+            Console.WriteLine("Book title : "+book.title);
+            Console.WriteLine("Book author : " + book.author );
+            Console.WriteLine("Book pages : " + book.pages);
+
+            Console.Read();
+        }
+    }
+}
